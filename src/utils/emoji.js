8d1b6e9cda2008f08f905d81d9e39552b5541121
ampcpmgp <ampcpmgp@gmail.com>
2018-05-30 00:29:31 +0900

    不要なログ除去

--- src/utils/emoji.js
+++ src/utils/emoji.js
@@ -18,17 +18,10 @@ export const parse = (text, lang) => {
         })
         const searchedIndex = index + 1
         index = text.indexOf(searchedWord, searchedIndex)
-        setTimeout(() => {
-          console.log(index, searchedIndex, text, searchedWord)
-        }, 0)
       } while (index > -1)
     }
     return result
   }, [])
-
-  setTimeout(() => {
-    console.log(parsed)
-  }, 0)
 
   return parsed
 }
